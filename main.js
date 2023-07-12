8dd5ebac116a2affa7e76f74efaaeed2ae268024
محمد رضا الصعيدي <reda23425@gmail.com>
2023-07-12 19:27:59 +0300

    .

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-$(function(){
-    $('#demo').hide();
-
-    $('#1').click(function(){
-        $('#demo').show();
+  var x = document.getElementById("imgs-search");
+  x.addEventListener("input", function(event) {
+    var searchValue = event.target.value.toLowerCase(); 
+    var images = document.querySelectorAll("#imgs img");
+    
+    images.forEach(function(image) {
+      var imageTitle = image.alt.toLowerCase(); 
+      
+      if (imageTitle.includes(searchValue)) 
+        image.style.display = "block"; 
+      else 
+        image.style.display = "none"; 
+      
     });
-
-
-
-
-});
+  });
